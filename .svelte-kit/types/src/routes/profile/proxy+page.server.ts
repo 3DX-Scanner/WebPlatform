175297341ec7367c3fbe40a6d28570cabaf4539d
Yoann <yoann.rohart@etu.mines-ales.fr>
2025-10-06 12:28:08 +0200

    arborescence projet

---
+++ .svelte-kit/types/src/routes/profile/proxy+page.server.ts
@@ -0,0 +1,12 @@
+// @ts-nocheck
+import type { PageServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
+
+export const load = async ({ locals }: Parameters<PageServerLoad>[0]) => {
+    if (!locals.user) {
+        throw redirect(302, '/login');
+    }
+    return {
+        user: locals.user
+    };
+};
